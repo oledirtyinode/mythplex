ffc4f136871904654889e8601f82c38ee5268644
Mike <mtodaro@gmail.com>
2016-01-22 14:43:01 -0600

    Add shell of postprocess.c. Nothing there for now.

---
+++ postprocess.c
@@ -0,0 +1,26 @@
+/* postprocess.c
+   (c) 2016 Mike Todaro, not for commercial use, all rights reserved
+   No warranties or gurantees, express or implied, are given by the author.
+   Use this software at your own risk! Any effect(s) it has on your systems
+   or anything else are not my responsibility!
+
+
+   postprocess.c is a replacement for my BASH script that takes MythTV
+   recordings and cleans them up and presents them to Plex for later 
+   viewing.
+*/
+
+#include <stdlib>
+#include <glib.h>
+
+int main(int argc, char *argv[])
+{
+
+   char[] chanid;
+   char[] airdate;  
+
+   
+   
+
+   return 0;
+}
